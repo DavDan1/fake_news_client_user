81bab3495e0295cd1484e69906fb081dd3f4041b
DavDan1 <david-danielyan@hotmail.com>
2021-05-13 14:32:19 +0200

    removes unnescessary comments

--- src/components/layout/BreakingNews.jsx
+++ src/components/layout/BreakingNews.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 
-const BreakingNews = ({firstArticle}) => {
-
+const BreakingNews = ({ firstArticle }) => {
   return (
     <div>
-      <div className='breaking-news' data-cy='breaking-news'  placeholder>
-        <img className='braking-img' src='https://images.unsplash.com/photo-1487758608033-7780b34680ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3589&q=80'/>
-        <div className="breaking-content">
-      {/* <h2>{firstArticle.title}</h2>
-        <h4>{firstArticle.teaser}</h4>
-        <p>{firstArticle.date}</p> */}
-
-        </div>
+      <div className='breaking-news' data-cy='breaking-news' placeholder>
+        <img
+          className='braking-img'
+          src='https://images.unsplash.com/photo-1487758608033-7780b34680ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3589&q=80'
+        />
+        <div className='breaking-content'></div>
       </div>
     </div>
   );
